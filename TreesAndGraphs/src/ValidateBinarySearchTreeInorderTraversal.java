f728e2b1fc97696b5eb03d12a00e281e8989fa96
Elliiee <danyiluozhang@gmail.com>
2020-01-15 10:07:37 -0600

    Validate Binary Search Tree Inorder Traversal

---
+++ TreesAndGraphs/src/ValidateBinarySearchTreeInorderTraversal.java
@@ -0,0 +1,30 @@
+import java.util.Stack;
+
+/*
+Validate Binary Search Tree
+Inorder Traversal
+ */
+public class ValidateBinarySearchTreeInorderTraversal {
+    public boolean isValidBST(TreeNode root){
+        Stack<TreeNode> stack = new Stack<>();
+        double inorder = -Double.MAX_VALUE;
+
+        while (!stack.isEmpty() || root != null){
+            while (root != null){
+                stack.push(root);
+                root = root.left; // this is a loop, so it will keep adding the left child
+            }
+            root = stack.pop();
+            if (root.val <= inorder)  return false;
+            inorder = root.val;
+            root = root.right;
+        }
+        return true;
+    }
+}
+
+/*
+For BST, inorder traversal means each element should be smaller than the next one.
+And also, we don't need to keep the whole list of inorder nodes, we only need the
+last added element.
+ */
