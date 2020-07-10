9fa0d2f062beb6720a6a8ad7434c3c1f88358c49
Elliiee <danyiluozhang@gmail.com>
2020-07-10 15:55:48 -0500

    07/10 Array and String 819

--- ArrayStringDesktop/src/_819MostCommonWord.java
+++ ArrayStringDesktop/src/_819MostCommonWord.java
@@ -44,4 +44,33 @@ public class _819MostCommonWord {
         }
         return Collections.max(count.entrySet(), Map.Entry.comparingByValue()).getKey();
     }
+
+    public String mostCommonWordIII(String paragraph, String[] banned){
+        // split paragraph
+        String[] words = paragraph.toLowerCase().split(("\\W+"));
+
+        // add banned words to set
+        Set<String> set = new HashSet<>();
+        for (String word : banned)
+            set.add(word);
+
+        // add paragraph words to hash map
+        Map<String, Integer> map = new HashMap<>();
+        for (String word : words){
+            if (!set.contains(word)){
+                map.put(word, map.getOrDefault(word, 0) + 1);
+            }
+        }
+
+        // get the most frequent word
+        int max = 0;
+        String res = "";
+        for (String s : map.keySet()){
+            if (map.get(s) > max){
+                max = map.get(s);
+                res = s;
+            }
+        }
+        return res;
+    }
 }
