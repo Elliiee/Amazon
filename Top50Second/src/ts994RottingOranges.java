ffe8df9fb1414c8373e91478b1a90bf994f2b098
Elliiee <danyiluozhang@gmail.com>
2020-07-28 15:14:28 -0500

    994

---
+++ Top50Second/src/ts994RottingOranges.java
@@ -0,0 +1,52 @@
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Set;
+
+public class ts994RottingOranges {
+    public int orangeRotting(int[][] grid){
+        if (grid == null || grid.length == 0) return 0;
+
+        int rows = grid.length;
+        int cols = grid[0].length;
+        Queue<int[]> queue = new LinkedList<>();
+        int count_fresh = 0;
+
+        for (int i = 0; i < rows; i++){
+            for (int j = 0; j < cols; j++){
+                if (grid[i][j] == 2){
+                    queue.offer(new int[]{i, j});
+                }
+                else if (grid[i][j] == 1){
+                    count_fresh++;
+                }
+            }
+        }
+
+        if (count_fresh == 0)
+            return 0;
+
+        int count = 0;
+        int[][] directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+
+        while (!queue.isEmpty()){
+            count++;
+            int size = queue.size();
+            for (int i = 0; i < size; i++){
+                int[] point = queue.poll();
+                for (int[] dir : directions){
+                    int x = point[0] + dir[0];
+                    int y = point[1] + dir[1];
+
+                    if (x < 0 || y < 0 || x >= rows || y >= cols || grid[x][y] == 0 || grid[x][y] == 2)
+                        continue;
+
+                    grid[x][y] = 2;
+                    queue.offer(new int[] {x, y});
+                    count_fresh--;
+                }
+            }
+        }
+        return count_fresh == 0 ? count - 1 : -1;
+    }
+}
